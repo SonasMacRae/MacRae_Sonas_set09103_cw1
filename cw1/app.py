f0cb176969ece7b0401d1e18f807cfe82fc258c9
Sonas MacRae <40277542@live.napier.ac.uk>
2018-10-20 22:04:38 +0100

    Update app.py

--- cw1/app.py
+++ cw1/app.py
@@ -13,53 +13,61 @@ gameInput = gameData["games"]
 
 @app.errorhandler(404)
 def page_not_found(error):
-	return render_template("errorpage.html"), 404
+        return render_template("errorpage.html"), 404
 
 # Route for the home page, which doubles as the page to display all of the developers
 @app.route("/developer/")
 @app.route("/")
 def home():
-	return render_template("home.html", developers=developerInput)
+        return render_template("home.html", developers=developerInput)
 
 # Returns a developers page with required console and developer data
 @app.route("/developer/<search>")
 def developer(search):
-	output = []
-	for d in developerInput:
-		if d["name"].lower() == search.lower():
-			output.append(d)
-	consoleList = []
-	for console in consoleInput:
-		if console["developer"].lower() == search.lower():
-			consoleList.append(console)
-	return render_template("developerpage.html", developer=output, consoleList=consoleList)
+        output = []
+        for d in developerInput:
+                if d["name"].lower() == search.lower():
+                        output.append(d)
+        consoleList = []
+        for console in consoleInput:
+                if console["developer"].lower() == search.lower():
+                        consoleList.append(console)
+        if len(output) == 0:
+                abort(404)
+        else:
+                return render_template("developerpage.html", developer=output, consoleList=consoleList)
 
 # Page that displays all of the consoles
 @app.route("/allconsoles/")
 def all():
-	return render_template("searchconsoles.html", consoleList=consoleInput)
+        return render_template("searchconsoles.html", consoleList=consoleInput)
 
 # Page that displays all of the consoles after a filter is applied
 @app.route("/allconsoles/<search>")
 def consoles(search):
-	consoleList = []
-	for console in consoleInput:
-		if console["developer"].lower() == search.lower():
-			consoleList.append(console)
-	return render_template("searchconsoles.html", consoleList=consoleList)
+        consoleList = []
+        for console in consoleInput:
+                if console["developer"].lower() == search.lower():
+                        consoleList.append(console)
+        if len(consoleList) == 0:
+                abort(404)
+        else:
+                return render_template("searchconsoles.html", consoleList=consoleList)
 
 # Page to display one console in more detail
 @app.route("/console/<search>")
 def console(search):
-	game = []
-	console = []
-	for c in consoleInput:
-		if c["name"].lower() == search.lower() or c["ref"].lower() == search.lower():
-			console.append(c)
-	for g in gameInput:
-		if g["id"].lower() == search.lower():
-			game.append(g)
-	return render_template("consoleprofile.html", console=console, game=game)
+        game = []
+        console = []
+        for c in consoleInput:
+                if c["name"].lower() == search.lower() or c["ref"].lower() == search.lower():
+                        console.append(c)
+        for g in gameInput:
+                if g["id"].lower() == search.lower():
+                        game.append(g)
+        if len(game) == 0 or len(console) == 0:
+                abort(404)
+        return render_template("consoleprofile.html", console=console, game=game)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', debug=True)
+        app.run(host='0.0.0.0', debug=True) 
